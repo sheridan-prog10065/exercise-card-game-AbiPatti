02a8b0371b452ee2fbbba8bed66e5e89f07cb8e2
Abinash <abinashpatti@gmail.com>
2025-04-04 15:15:39 -0400

    Implemented UI for dealing cards

--- CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameInteractive/MainPage.xaml.cs
@@ -14,10 +14,25 @@ public partial class MainPage : ContentPage
 
     private void OnDealCards(object sender, EventArgs e)
     {
+        // Make sure the cards are face down
+        _imgPlayerCard.Source = s_imageSourceCardBack();
+        _imgHouseCard.Source = s_imageSourceCardBack();
 
         // Deal cards to the player and the house
         _cardGame.DealCards();
 
+        // Inform the user what they can do next and change usability of buttons
+        _txtGameBoard.Text = "You can now play or switch cards.";
+
+        _btnDealCards.IsEnabled = false;
+        _btnSwitchCards.IsEnabled = true;
+        _btnPlayCards.IsEnabled = true;
+
+    }
+
+    private static ImageSource s_imageSourceCardBack()
+    {
+        return ImageSource.FromFile("playing_card_back.jpg");
     }
 
     private void OnSwitchCards(object sender, EventArgs e)
